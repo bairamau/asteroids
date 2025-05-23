57dfcb6a9d9668b715191ae9b971b46b62c5ec03
Zaur Bairamau <bairamau.z@gmail.com>
2025-05-23 22:27:34 +0400

    add player shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -11,11 +12,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        updatable.update(dt)
+        for shape in drawable:
+            shape.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
